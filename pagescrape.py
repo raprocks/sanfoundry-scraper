b2321b5a0d9830e781a7856f0e6f2875a70ba0c4
Rohit Patil <rahulhimesh09@gmail.com>
2021-03-13 01:40:50 +0530

    now pulls only tables to mcq pages

--- pagescrape.py
+++ pagescrape.py
@@ -1,12 +1,15 @@
+from typing import Dict
 import requests
 from bs4 import BeautifulSoup
 
 
-def pagescrape(url: str):
+def pagescrape(url: str) -> Dict[str, str]:
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'lxml')
     content = soup.find('div', class_='entry-content')
-    tables = content.findAll('table')
+    sf_contents = content.findAll('div', class_='sf-section')
+    filtered_sf_content = [item for item in sf_contents if item.h2 is not None and item.table is not None]
+    tables = [item.table for item in filtered_sf_content]
     links = {}
     for table in tables:
         hrefs = {link.text.strip().replace(
@@ -17,5 +20,6 @@ def pagescrape(url: str):
 
 if __name__ == '__main__':
     results = pagescrape(
-        "https://www.sanfoundry.com/1000-object-oriented-programming-oops-questions-answers/")
+        "https://www.sanfoundry.com/1000-object-oriented\
+            -programming-oops-questions-answers/")
     print(results)
