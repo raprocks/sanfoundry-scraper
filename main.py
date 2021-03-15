1bdfcfcbd1d6a58950b3fd011916f86b04eedc36
Rohit Patil <rahulhimesh09@gmail.com>
2021-03-16 00:49:00 +0530

    made the main file so that it saved mcqs in one single file

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from pagescrape import pagescrape
-from mcqscrape import mcqscrape_html
 
-PAGE_URL = "https://www.sanfoundry.com/1000-object-oriented-programming-oops-questions-answers/"
+from pagescrape import pagescrape
+from mcqscrape import mcqscrape_html, write_to_html
+from bs4 import BeautifulSoup
+PAGE_URL = "https://www.sanfoundry.com/1000-object\
+    -oriented-programming-oops-questions-answers/"
 
 pages = pagescrape(PAGE_URL)
-down_data = {}
+mega_html = ''
 with open('downloaded.json', 'w+') as fd:
     for k, v in pages.items():
         print("getting", k, "from ->", v, end=' ... ')
-        mcqscrape_html(v, PAGE_URL.split('/')[-2])
+        mega_html += mcqscrape_html(v)
         print("Done!")
+    write_to_html(BeautifulSoup(mega_html, 'lxml').prettify(),
+                  PAGE_URL.split('/')[-2])
